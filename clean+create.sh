281b39a680533a164103e1a00821d5863aafb059
andrewcgaitskell <andrew@gaitskell.com>
2024-08-15 16:35:34 +0100

    Update clean+create.sh

--- clean+create.sh
+++ clean+create.sh
@@ -1,1 +1,14 @@
+podman stop publish_dash_component_container
+podman rm publish_dash_component_container
+
+podman rmi publish_dash_component_image
+
+podman build \
+--ulimit=nofile=4096:4096 \
+-f Dockerfile -t publish_dash_component_image
+
+podman create \
+--name frontend_container \
+--log-opt max-size=10mb \
+localhost/publish_dash_component_image:latest
 
